91c7834213a713dd5b037a7c981319327e8f2b03
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2022-12-03 17:06:28 +0000

    Display addition questions to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,19 +17,35 @@ document.addEventListener("DOMContentLoaded", function() {
                 // Create an alert if game buttons clicked
             }   else { 
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);            
+                // Let the gamme to run when buttom loaded 
+                runGame(gameType);
             }
         })
     }
+
+    // Set addition as the defualt game
+    runGame("addition");
 })
 /**
  * The main game "loop", called when the script is first oladed
  * and after the user's answer has been processed
  */
-function runGame() {
+// Add parameters to runGame that is gameType
+function runGame(gameType) {
+
     // Create 2 random numbers between 1 and 25
     let num1 = math.floor(math.random() * 25) +1;
     let num2 = math.floor(math.random() * 25) +1;
+
+    // check which game type is running using if statment
+    // call appropriate function
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+        else {
+            alert(`Unkown game type: ${gameType}`);
+            throw `Unkown game type: ${gameType}. Aborting!`;
+        }
+    }
 
 }
 
@@ -49,8 +65,11 @@ function incrementWrongAnswer () {
 
 }
 
-function displayAdditionQuestion () {
-
+// interrogating html and calling elements that have operand1, operand2 and operator
+function displayAdditionQuestion (operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion () {
